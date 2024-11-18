a326e0b0566b47418c088908aa7bb5f6e67eed9e
Artem Arafailov <artem.ar888@gmail.com>
2024-11-18 18:50:48 +0300

    Variables have been renamed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,6 +1,6 @@
 public class CreditPaymentService {
-    public int calculate (int s, int t, double p) {
-        int annuityPayment = (int) Math.round(s * (p / (12 * 100)) / (1 - Math.pow((1 + (p / (12 * 100) )), -t)));
+    public int calculate(int loanAmount, int loanTerm, double interestRate) {
+        int annuityPayment = (int) Math.round(loanAmount * (interestRate / (12 * 100)) / (1 - Math.pow((1 + (interestRate / (12 * 100))), -loanTerm)));
         return annuityPayment;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        int a = 1_000_000; // сумма кредита в руб.
-        int b = 12; // срок кредита в мес.
-        double c = 9.99; // процентная ставка в %
-        System.out.printf("\n Сумма кредита: %,d руб. \n Срок кредита: %s мес.  \n Процентная ставка: %s %% \n", a, b, c);
-        System.out.printf("Ежемесячный платёж: %,d руб.\n", service.calculate(a, b, c));
+        int loanAmount = 1_000_000; // сумма кредита в руб.
+        int loanTerm = 12; // срок кредита в мес.
+        double interestRate = 9.99; // процентная ставка в %
+        System.out.printf("\n Сумма кредита: %,d руб. \n Срок кредита: %s мес.  \n Процентная ставка: %s %% \n", loanAmount, loanTerm, interestRate);
+        System.out.printf("Ежемесячный платёж: %,d руб.\n", service.calculate(loanAmount, loanTerm, interestRate));
 
-        a = 1_000_000; // сумма кредита в руб.
-        b = 24; // срок кредита в мес.
-        c = 9.99; // процентная ставка в %
-        System.out.printf("\n Сумма кредита: %,d руб. \n Срок кредита: %s мес.  \n Процентная ставка: %s %% \n", a, b, c);
-        System.out.printf("Ежемесячный платёж: %,d руб.\n", service.calculate(a, b, c));
+        loanAmount = 1_000_000; // сумма кредита в руб.
+        loanTerm = 24; // срок кредита в мес.
+        interestRate = 9.99; // процентная ставка в %
+        System.out.printf("\n Сумма кредита: %,d руб. \n Срок кредита: %s мес.  \n Процентная ставка: %s %% \n", loanAmount, loanTerm, interestRate);
+        System.out.printf("Ежемесячный платёж: %,d руб.\n", service.calculate(loanAmount, loanTerm, interestRate));
 
-        a = 1_000_000; // сумма кредита в руб.
-        b = 36; // срок кредита в мес.
-        c = 9.99; // процентная ставка в %
-        System.out.printf("\n Сумма кредита: %,d руб. \n Срок кредита: %s мес.  \n Процентная ставка: %s %% \n", a, b, c);
-        System.out.printf("Ежемесячный платёж: %,d руб.\n", service.calculate(a, b, c));
+        loanAmount = 1_000_000; // сумма кредита в руб.
+        loanTerm = 36; // срок кредита в мес.
+        interestRate = 9.99; // процентная ставка в %
+        System.out.printf("\n Сумма кредита: %,d руб. \n Срок кредита: %s мес.  \n Процентная ставка: %s %% \n", loanAmount, loanTerm, interestRate);
+        System.out.printf("Ежемесячный платёж: %,d руб.\n", service.calculate(loanAmount, loanTerm, interestRate));
     }
 }
